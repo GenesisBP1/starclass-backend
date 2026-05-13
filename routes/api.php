224b10a6efa73367e0ff41246b8936ed1a8e30ae
Genesis Atenea Burela Pintado <l22260140@matamoros.tecnm.mx>
2026-05-13 17:53:46 -0600

    2025.05.13 correccion en api routes para verificacion de tokens

--- routes/api.php
+++ routes/api.php
@@ -9,33 +9,54 @@ use App\Http\Controllers\Api\AsistenciaController;
 use App\Http\Controllers\Api\QrController;
 use App\Http\Controllers\Api\ExportController;
 
-
+/*
+|--------------------------------------------------------------------------
+| Rutas públicas
+|--------------------------------------------------------------------------
+*/
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/clases', [ClaseController::class, 'store']);
-Route::get('/clases/maestro/{id}', [ClaseController::class, 'clasesMaestro']);
-Route::get('/clases', [ClaseController::class, 'index']);
-Route::get('/clases/alumno/{id}', [ClaseController::class, 'clasesAlumno']);
-Route::get('/clases/{id}', [ClaseController::class, 'show']);
-Route::put('/clases/{id}', [ClaseController::class, 'update']);
-Route::delete('/clases/{id}', [ClaseController::class, 'destroy']);
-Route::post('/clases/unirse', [ClaseController::class, 'unirse']);
-Route::post('/tareas', [TareaController::class, 'store']);
-Route::get('/clases/{id}/tareas', [TareaController::class, 'tareasPorClase']);
-Route::post('/tareas/entregar', [EntregaController::class, 'entregar']);
-Route::get('/tareas/{id}/entregas', [EntregaController::class, 'entregasPorTarea']);
-Route::post('/asistencias/registrar', [AsistenciaController::class, 'registrar']);
-Route::get('/clases/{id}/asistencias', [AsistenciaController::class, 'asistenciasPorClase']);
-Route::post('/qr/generar', [QrController::class, 'generar']);
-Route::post('/qr/validar', [QrController::class, 'validar']);
-Route::put('/clases/{id}', [ClaseController::class, 'actualizar']);
-Route::delete('/clases/{id}', [ClaseController::class, 'eliminar']);
-Route::put('/tareas/{id}', [TareaController::class, 'actualizar']);
-Route::delete('/tareas/{id}', [TareaController::class, 'eliminar']);
-Route::get('/clases/{id}/alumnos', [ClaseController::class, 'alumnosClase']);
-Route::get('/tareas/{id}/reporte', [EntregaController::class, 'reporteTarea']);
-Route::get('/clases/{id}/reporte-tareas', [TareaController::class, 'reporteTareasClase']);
-Route::get('/export/asistencias/{id}', [ExportController::class, 'exportarAsistencias']);
-Route::get('/export/entregas/{id}', [ExportController::class, 'exportarEntregas']);
-Route::get('/export/tareas/{id}', [ExportController::class, 'exportarTareas']);
 
+/*
+|--------------------------------------------------------------------------
+| Rutas protegidas con Sanctum
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Clases
+    Route::post('/clases', [ClaseController::class, 'store']);
+    Route::get('/clases', [ClaseController::class, 'index']);
+    Route::get('/clases/maestro/{id}', [ClaseController::class, 'clasesMaestro']);
+    Route::get('/clases/alumno/{id}', [ClaseController::class, 'clasesAlumno']);
+    Route::get('/clases/{id}', [ClaseController::class, 'show']);
+    Route::put('/clases/{id}', [ClaseController::class, 'actualizar']);
+    Route::delete('/clases/{id}', [ClaseController::class, 'eliminar']);
+    Route::post('/clases/unirse', [ClaseController::class, 'unirse']);
+    Route::get('/clases/{id}/alumnos', [ClaseController::class, 'alumnosClase']);
+
+    // Tareas
+    Route::post('/tareas', [TareaController::class, 'store']);
+    Route::get('/clases/{id}/tareas', [TareaController::class, 'tareasPorClase']);
+    Route::put('/tareas/{id}', [TareaController::class, 'actualizar']);
+    Route::delete('/tareas/{id}', [TareaController::class, 'eliminar']);
+    Route::get('/clases/{id}/reporte-tareas', [TareaController::class, 'reporteTareasClase']);
+
+    // Entregas
+    Route::post('/tareas/entregar', [EntregaController::class, 'entregar']);
+    Route::get('/tareas/{id}/entregas', [EntregaController::class, 'entregasPorTarea']);
+    Route::get('/tareas/{id}/reporte', [EntregaController::class, 'reporteTarea']);
+
+    // Asistencias
+    Route::post('/asistencias/registrar', [AsistenciaController::class, 'registrar']);
+    Route::get('/clases/{id}/asistencias', [AsistenciaController::class, 'asistenciasPorClase']);
+
+    // QR
+    Route::post('/qr/generar', [QrController::class, 'generar']);
+    Route::post('/qr/validar', [QrController::class, 'validar']);
+
+    // Exportaciones
+    Route::get('/export/asistencias/{id}', [ExportController::class, 'exportarAsistencias']);
+    Route::get('/export/entregas/{id}', [ExportController::class, 'exportarEntregas']);
+    Route::get('/export/tareas/{id}', [ExportController::class, 'exportarTareas']);
+});
